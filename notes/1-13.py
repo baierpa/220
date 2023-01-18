d6f90ccf8b2ff42151493a987b41e407165d966c
baierpa <baierpa@cofc.edu>
2023-01-18 10:25:21 -0500

    notes 1-18

--- notes/1-13.py
+++
@@ -1,5 +0,0 @@
-print('hello, world!')
-print(3 + 7)
-
-# 3 + 7 expression
-# print(3 + 7) statement
---
+++ notes/ch1.py
@@ -0,0 +1,30 @@
+greeting = "hello, world!"
+
+print(greeting)
+print('greeting')
+
+my_result = 3 + 7 - 9 * 2
+resultB = 3 ** 2
+print(my_result + resultB)
+
+first_name = 'ann'
+last_name = 'smith'
+
+print('Ms.', first_name, last_name)
+
+# color = input("Enter your favorite color: ")
+# print('Oh, I like', color, 'too!')
+
+age = input("enter your age: ")
+int_age = eval(age)
+ten_years = int_age + 10
+print("in 10 years you will be", ten_years)
+
+# 3 + 7 expression
+# print(3 + 7) statement
+
+# Data Types:
+# Strings (str) - require quotes ''
+# Integers (int)
+#   Math Operators:  + - * / **
+#   PEMDAS applies!
