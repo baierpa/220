c247f2de1a84b5c0acdd04a1d1c6e4ca7bc53150
baierpa <baierpa@cofc.edu>
2023-01-18 11:32:05 -0500

    notes 1-18

---
+++ notes/ch1.py
@@ -0,0 +1,28 @@
+# This is a comment
+greeting = 'hello, world!'
+print(greeting)
+values_a = 3 + 7 - 2 * 9
+values_b = 3 ** 2
+print(values_a - values_b)
+# print('how are you?')
+# 3 + 7
+
+first_name = 'ann'
+last_name = 'smith'
+print('ms.', first_name, last_name)
+
+# color = input('Enter your favorite color: ')
+# print('i really like', color, 'too!')
+
+age = input('Enter your age: ')
+int_age = eval(age)
+ten_years = int_age + 10
+print('in ten years you will be', ten_years)
+
+
+# Data Types
+# String (str) - anything in quotes ''
+# Integer (int) - whole numbers!
+#   math operators - + - * / **
+#   Order of Operations - PEMDAS
+
