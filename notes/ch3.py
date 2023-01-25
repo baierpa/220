7b646de6c6c0b6445e72f62a7ebd5c5fcfd833f0
baierpa <baierpa@cofc.edu>
2023-01-25 11:24:03 -0500

    notes 01-25

--- notes/ch3.py
+++ notes/ch3.py
@@ -1,3 +1,6 @@
+# libraries
+import math
+
 # print
 # input
 
@@ -15,4 +18,39 @@ start_stop_step = range(1, 10, 2) # generate 1 - 9
 sum = 0
 for num in stop:
     sum = sum + num
-print(sum)
+# print(sum)
+
+one_to_ten = range(1, 11)
+# for one_number in one_to_ten:
+#     # body
+#     print(one_number)
+# print('done!')
+
+ten_range = range(100, 110)
+for i in ten_range:
+    print('hey!', end=' ')
+print()
+
+# age = 43
+# next_ten = range(1, 11)
+# for year in next_ten:
+#     new_age = age + year
+#     print('after', year, 'year(s) you will be', new_age, 'years old!')
+# print('wow, you will be really old after 10 years!')
+# print(age)
+#
+# for j in range(10):
+#     age = age + 1
+#     print(age)
+# print(age)
+
+# nested loops
+one_to_five_outer = range(1, 11)
+one_to_five_inner = range(1, 5)
+for i in one_to_five_outer:
+    for j in one_to_five_inner:
+        print(i + j, end='\t')
+    print()
+
+# + - * / **
+math.sqrt(9)
