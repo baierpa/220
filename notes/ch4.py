e257d25aa6772fd96dc9a1fdf598cd50a54aa674
baierpa <baierpa@cofc.edu>
2023-02-03 10:25:25 -0500

    notes 02-03

--- notes/ch4.py
+++ notes/ch4.py
@@ -1,4 +1,4 @@
-from graphics import Circle, Point
+from graphics import Circle, Point, GraphWin
 
 # import math
 from math import sqrt, pow
@@ -10,4 +10,18 @@ from math import sqrt, pow
 
 age = 7
 name = "john"
-my_point = Point(2, 9)
+center = Point(2, 9)
+my_circle = Circle(center, 70)
+title = "My Cool Window"
+win = GraphWin(title, 700, 500)
+
+point_x_value = center.getX()
+circle_center = my_circle.getCenter()
+circle_center.getX()
+circle_center.getY()
+my_circle.draw(win)
+
+input()
+
+# Instance Variables - Values that are stored within a data type
+# Object - variable that holds data
