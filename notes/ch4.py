e2afaa413746adfd18b04feb51fcf727bf5fd7c2
baierpa <baierpa@cofc.edu>
2023-02-03 11:26:24 -0500

    notes 02-03

--- notes/ch4.py
+++ notes/ch4.py
@@ -1,4 +1,4 @@
-from graphics import Point, Circle
+from graphics import Point, Circle, GraphWin
 
 # import math
 # from math import sqrt, pow
@@ -8,3 +8,17 @@ from graphics import Point, Circle
 # math.sqrt(9)
 # sqrt(9)
 
+age = 7
+name = "john"
+last_name = "smith"
+my_point = Point(200, 300)
+center = Point(100, 600)
+my_circle = Circle(center, 150)
+title = "My Cool Window!"
+win = GraphWin(title, 700, 500)
+
+the_x_value = my_point.getX()
+print(the_x_value)
+print(my_circle.getY())
+
+# input('just waiting...')
