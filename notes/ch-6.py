25b22df2dc676d2193630abc7c81320510bd48d7
baierpa <baierpa@cofc.edu>
2023-02-22 12:36:00 -0500

    notes 02-22

---
+++ notes/ch-6.py
@@ -0,0 +1,20 @@
+# Functions!
+import math
+
+# answer = sqrt_9()
+# answer = sqrt_16()
+answer = math.sqrt(9)
+
+# name
+# parameters
+# purpose
+# return type
+
+# function definition / signature
+
+def print_number_plus_one(x):
+    new_number = x + 1
+    return new_number
+
+
+new_value = print_number_plus_one(7)
