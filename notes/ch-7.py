bd12f7d4ad55de72b5271024b57291d8b58dff2a
baierpa <baierpa@cofc.edu>
2023-03-03 11:20:28 -0500

    notes 03-03

--- notes/ch-7.py
+++ notes/ch-7.py
@@ -1,3 +1,6 @@
+def is_even(num):
+    return num % 2 == 0
+
 # Control Structures
 x = True
 y = False
@@ -5,5 +8,16 @@ print(x, y)
 # if condition:
 #   body
 
+
 if False:
     print('hello')
+
+age = 10
+if is_even(age):
+    print('that is an even number!')
+
+# relational operators
+# < <= > >= == != not
+x = 123
+if not(9 != 9):
+    print("hi")
