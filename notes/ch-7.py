fa56c940d6b46492c640b8e13b0db8e60389700c
baierpa <baierpa@cofc.edu>
2023-03-03 10:22:25 -0500

    notes 03-03

--- notes/ch-7.py
+++ notes/ch-7.py
@@ -1,18 +1,50 @@
 # Control Structures
 # Conditional
+# booleans
 x = True
 y = False
 # if condition:
 #   body
-def my_boolean_check():
-    return False
 
 
-if my_boolean_check():
-    print('hello')
+if y:
+    print('yay!')
+
+def is_even(num):
+  return num % 2 == 0
+
+if is_even(4):
+    print('yeah, that is even!')
 
 # Relational Operators
-# < > <= >=
+# < > <= >= == != not
 print(3 < 7)
-if 3 < 7:
-    print('less than!')
+if 7 == 7:
+    print('ok')
+
+# get campers ages
+# for each camper check if even/odd
+# if even, move left
+# if odd, move right
+
+from random import randint
+
+def get_campers():
+    campers = []
+    number_of_campers = range(50)
+    for i in number_of_campers:
+        random_age = randint(8, 16)
+        campers.append(random_age)
+    return campers
+
+all_campers = get_campers()
+left = []
+right = []
+for camper in all_campers:
+    if camper % 2 == 0:
+        left.append(camper)
+    if camper % 2 == 1:
+        right.append(camper)
+print(left)
+print(right)
+
