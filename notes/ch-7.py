be84ba5e5c96abad7e27d65011b812db5cbee81c
baierpa <baierpa@cofc.edu>
2023-03-03 12:47:14 -0500

    notes 03-03

--- notes/ch-7.py
+++ notes/ch-7.py
@@ -1,5 +1,8 @@
+def is_even(num):
+    return num % 2 == 0
 # Control Structures
 
+# boolean values
 x = True
 y = False
 
@@ -9,4 +12,12 @@ y = False
 if False:
     print("hello")
 
-print('always')
+age = 23
+if is_even(age):
+    print("age is even!")
+
+# realtional operators
+# < > <= >= == != not
+if not(3 < 5):
+    print("less than!")
+
