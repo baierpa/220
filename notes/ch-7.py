901c9c1146ced536b6c0f1edb0aefec06baa24ad
baierpa <baierpa@cofc.edu>
2023-03-13 11:24:26 -0400

    notes 03-13

--- notes/ch-7.py
+++ notes/ch-7.py
@@ -1,3 +1,5 @@
+from random import randint
+
 def is_even(num):
     return num % 2 == 0
 
@@ -21,3 +23,29 @@ if is_even(age):
 x = 123
 if not(9 != 9):
     print("hi")
+
+def get_campers():
+    # campers ages
+    campers = []
+    total_campers = range(50)
+    for i in total_campers:
+        random_age = randint(8, 18)
+        campers.append(random_age)
+    return campers
+
+campers = get_campers()
+caterpillars = [] # [0-10)
+butterflies = [] # [10-13)
+eagles = [] # [13-18]
+
+for camper_age in campers:
+    if camper_age < 10:
+        caterpillars.append(camper_age)
+    elif camper_age < 13:
+        butterflies.append(camper_age)
+    else:
+        eagles.append(camper_age)
+
+print(caterpillars)
+print(butterflies)
+print(eagles)
