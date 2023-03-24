705146b92ca310d2d2c7c95bc522a9f5be8a5e5c
baierpa <baierpa@cofc.edu>
2023-03-24 11:21:36 -0400

    notes 03-24

---
+++ notes/ch10.py
@@ -0,0 +1,16 @@
+# DICE
+# Instance Varibales - the data! what is it? what does it have?
+#   - sides
+#   - side values
+#   - order
+#   - shape
+#   - value
+# Methods - actions. what can it do?
+#   - roll
+#   - get value
+#   - get sides
+#   - set sides
+#   - set value
+# Constructor - how is a die created?
+#   ex: d = Dice(6)
+#   - number of sides
--- notes/ch8-2.py
+++ notes/ch8-2.py
@@ -24,10 +24,57 @@ def circle_draw():
         if key == 'q':
             break
 
+
 # short circuiting
 x = 5
 x > 7 and math.sqrt(-1)
 
-x = [False]
-if x:
-    print('ok')
+
+def truthy_falsy():
+    x = [False]
+    if x:
+        print('ok')
+
+    x = 3 > 7 or False
+    print(x)
+
+    # T and T
+    x = 7 and "hello"
+    print(x)
+    # T and F
+    x = "hey" and []
+    print(x)
+    # F and T
+    x = 0 and [1, 2, 3, 4, 5]
+    print(x)
+    # F and F
+    x = [] and ''
+    print(x)
+
+    # T or T
+    x = 99 or ['hello', 'world!']
+    print(x)
+    # T or F
+    x = True or ''
+    print(x)
+    # F or T
+    x = '' or [1, 2, 3]
+    print(x)
+    # F or F
+    x = '' or 0
+    print(x)
+
+
+def transfer():
+    answer = input('do you want to transfer all of your money to us (yes/no)? ')
+    first_letter = answer[0]
+    if first_letter == 'Y' or first_letter == 'y':
+        print('great, thanks! transferring...')
+    else:
+        print('whatever. see ya')
+
+
+def ice_cream():
+    order = input("what ice cream do you want [default is vanilla]? ")
+    final_order = order or 'vanilla'
+    print('one', final_order, 'ice cream comin up!')
