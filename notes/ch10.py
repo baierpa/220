9309e2e931b544286bd8039cefa860ea01d1a5f8
baierpa <baierpa@cofc.edu>
2023-03-24 10:20:14 -0400

    notes 03-24

---
+++ notes/ch10.py
@@ -0,0 +1,12 @@
+# DICE
+# Instance Variables - what is it/what does it have?
+#   - has a number of sides
+#   - side values
+#   - probabilites for each sides
+#   - value
+# Methods - what can it do?
+#   - roll
+#   - get value
+# Constructor - how do we make it?
+#   - number of sides
+#   my_die = Dice(6)
--- notes/ch8-2.py
+++ notes/ch8-2.py
@@ -27,11 +27,43 @@ def circle_mover():
 # and or
 result = False and math.sqrt(-1)
 
-x = 1 and "hi"
-print(x)
-x = 1 and ''
-print(x)
-x = 0 and True
-print(x)
+def truthy_falsy():
+    x = True and False
+    print(x)
+    # T and T
+    x = 1 and "hi"
+    print(x)
+    # T and F
+    x = "hey!" and []
+    print(x)
+    # F and T
+    x = 0 and [1,2,3,4]
+    print(x)
+    # F and F
+    x = "" and 0
+    print(x)
 
-# can a point have a truthy value?
+    # T or T
+    x = "hi" or 9
+    print(x)
+    # T or F
+    x = [1,2,3,4,5] or ""
+    print(x)
+    # F or T
+    x = False or 87
+    print(x)
+    # F or F
+    x = 0 or False
+    print(x)
+
+def transfer():
+    answer = input('do you want to transfer all of you money to us (yes or no)? ')
+    first_letter = answer[0]
+    if first_letter == 'Y' or first_letter == 'y':
+        print('thank you! transferring...')
+    else:
+        print('ok. see ya.')
+
+ans = input('what is your favorite icecream (default is vanilla)? ')
+ans = ans or 'vanilla'
+print("oh, i like", ans, 'too!')
