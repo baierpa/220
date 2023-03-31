49636a426f391309bbef07321d4b65d1e78048b4
baierpa <baierpa@cofc.edu>
2023-03-31 12:20:30 -0400

    notes 03-31

--- notes/game.py
+++ notes/game.py
@@ -24,16 +24,52 @@ class Dice:
         return randint(1, self.sides)
 
 class DiceGame:
+    def __init__(self, winning_score, num_players, dice):
+        self.round = 1
+        self.winning_score = winning_score
+        self.dice = dice
+        self.scores = []
+        self.players = []
+        player_range = range(num_players)
+        # player registration
+        for number in player_range:
+            self.scores.append(0)
+            self.players.append(f'player{number + 1}')
+
     # constructor - how to create a DiceGame
-    # game = DiceGame(winning_score, num_players)
+    # game = DiceGame(winning_score, num_players, num_dice)
     # dice?
 
     # instance variables - what data is in a DiceGame - What is it?
-    #   - player scores - list[int]
+    #   - player scores - list[int] ex: [0, 0, ...]
     #   - dice - list[Dice]
     #   - round - int
     #   - winning score - int
-    #   - players - list[str]
+    #   - players - list[str] ex: ["player1", "player2", ...]
+
+    def play(self):
+        while not self.game_over():
+            # test game over!
+            print(self.scores)
+        # while the game not over
+        #   print the round
+        #   for each player
+        #       print the player
+        #       player rolls (hits enter)
+        #       for each dice
+        #           roll
+        #           get/print the value
+        #           add value to sum
+        #       print total
+        #       add total to player's score
+        #       check if the game is over
+        #   print game summary
+
+    def game_over(self):
+        for score in self.scores:
+            if score >= self.winning_score:
+                return True
+        return False
 
     # methods -  what can a DiceGame do?
     #   - tally scores
---
+++ notes/game_tester.py
@@ -0,0 +1,8 @@
+from game import DiceGame, Dice
+
+d1 = Dice(6)
+d2 = Dice(8)
+dice = [d1, d2]
+
+my_game = DiceGame(20, 2, dice)
+my_game.play()
