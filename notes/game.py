f5b7589a96b67b17b6bcf38df8841ddfe68d7a24
baierpa <baierpa@cofc.edu>
2023-03-31 11:20:36 -0400

    notes 03-31

--- notes/game.py
+++ notes/game.py
@@ -25,14 +25,51 @@ class Dice:
 
 class DiceGame:
     # constructor - how to create/start the game
-    # game = DiceGame(num_players, winning_score, list_dice )
+    def __init__(self, number_players, winning_score, dice):
+        self.round = 1
+        self.winning_score = winning_score
+        self.dice = dice
+        self.players = []
+        self.scores = []
+        player_range = range(number_players)
+        # register players and start scores at 0
+        for i in player_range:
+            self.players.append(f'player{i + 1}')
+            self.scores.append(0)
+
+    def play(self):
+        while not self.__game_over():
+            print(f'round {self.round}')
+            self.scores[3] = self.scores[3] + 2
+            print(self.scores)
+            self.round = self.round + 1
+        # while the game is not over
+        #   print the round
+        #   for each player
+        #       tell them to roll
+        #       roll the dice
+        #       for each die
+        #             print its value
+        #             sum the values
+        #         print the total roll
+        #         check if the game is over
+        #   print game report
+
+    def __game_over(self):
+        for score in self.scores:
+            if score >= self.winning_score:
+                return True
+        return False
+
+    # game = DiceGame(num_players, winning_score, list_dice)
     #   - number of players
     #   - winning score
-    #   - number of dice
+    #   - list of dice
+    #   - rounds
 
     # instance variables - the data stored in the game | what is a game?
     #   - players - list[str] ex: ['player1', 'player2', ...]
-    #   - scores - list[int]
+    #   - scores - list[int] ex: [0, 0, 0, ...]
     #   - winning score - int
     #   - dice - list[Dice] ex: [Dice(6), Dice(8), ...]
     #   - rounds - int
@@ -43,4 +80,3 @@ class DiceGame:
     #   - check player turn
     #   - report scores
     #   - play
-    pass
---
+++ notes/game_tester.py
@@ -0,0 +1,7 @@
+from game import DiceGame, Dice
+
+d1 = Dice(6)
+d2 = Dice(8)
+game_dice = [d1, d2]
+my_game = DiceGame(4, 25, game_dice)
+my_game.play()
