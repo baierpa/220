c859928b634d87d229670d09a79cb94368566c2e
baierpa <baierpa@cofc.edu>
2023-04-12 11:13:30 -0400

    notes 04-12

--- notes/ch11-2.py
+++ notes/ch11-2.py
@@ -52,10 +52,72 @@ print(more_ages * 3)
 # {} - dictionary
 
 names_list = ['wyatt', 'wendy', 'willy']
-names_dictionary = {10000:'wyatt', 200:'wendy', 300:'willy'}
+names_dictionary = {10000:'wyatt', 200:'wendy', 300:'willy'} # key:value
 
+# referencing values
 print(names_list[0])
 print(names_dictionary[10000])
 
 families = {'smith': ['susan', 'sam', 'steve']}
-print(families['smith'])
+# {'smith': ['susan', 'sam', 'steve']}
+print(families)
+names = families['smith']
+print(names)
+# insert into dictionary
+families['jones'] = ['jerry', 'jane', 'jim']
+# {'smith': ['susan', 'sam', 'steve'], 'jones': ['jerry', 'jane', 'jim']}
+print(families)
+families['able'] = ['aaron', 'ada', 'al']
+
+# in
+has_smith = 'smith' in families
+if has_smith:
+    print('hey smith family!')
+
+last_name = 'jones'
+print(families[last_name])
+
+# keys(), values(), items()
+# keys() - return a sequence of keys from the dictionary
+last_names = families.keys()
+for last_name in last_names:
+    value = families[last_name]
+    print(f'Introducing the {last_name} family!')
+    print(f'\t{value}')
+
+# last_names_list = list(last_names)
+# smith = last_names_list[0]
+# print(smith)
+
+# values() - return a sequence of values from the dictionary
+names = families.values()
+for name in names:
+    for person in name:
+        print(person, end='---')
+    print()
+names_list = list(names)
+names_list[0]
+
+# items - return a sequence of tuples containing key, value pairs
+familiy_members = families.items()
+for family in familiy_members:
+    last_name = family[0]
+    members = family[1]
+    print(f'The {last_name} family!')
+    print(f'\t{members}')
+
+for family in families:
+    print(f'{family}!!!')
+
+# families.clear()
+# print(families)
+
+# del(families['jones'])
+# print(families)
+
+ables = families.get('ables',  ['a', 'b', 'c'])
+print(ables)
+stocks = {'abc': None}
+print(stocks)
+stocks['abc'] = 'something'
+print(stocks)
